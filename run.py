d6d54895c8de158438ea8b16a730bf661e2759c7
Maria Eduarda <165069113+MariaGomesR@users.noreply.github.com>
2025-05-30 14:16:17 -0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, redirect, request 
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+app.run(host='0.0.0.0', port=5000, debug=True)
